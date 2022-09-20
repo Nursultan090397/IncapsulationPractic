7da8b5a3105f0e124090558ba0ca6ca80e177cdc
osorov97 <osorov.nursultan97@gmail.com>
2022-09-20 13:18:53 +0600

    Initial commit

---
+++ src/SafeIntance.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Car Porsche = new Car();
+        Porsche.setCar(" Porsche ", " Красный ", " Купе ");
+        Porsche.g
+    }
+}
